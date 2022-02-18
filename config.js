1718ea19e844495f42c6b9187b6ff52b31c1e3f6
Sans <99873347+r3ngan@users.noreply.github.com>
2022-02-18 23:10:00 +0100

    🌊v1.0

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const token = "OTM0MTcxNTUzMjY3MDU2NzUw.Yg4L4g.zPjz_U2gb8PYuL5eUh7XMnqu8gE"; //ici met ton token ce qui permettra au selfbot de fonctionner
-const prefix = "s"; //le prefix est la chose que tu devras mettre avant une cmd
+const token = ""; //ici met ton token ce qui permettra au selfbot de fonctionner
+const prefix = "."; //le prefix est la chose que tu devras mettre avant une cmd
 const color = "00ff00"; //une couleur HTML
 const image = ""; //met l'url d'une image que tu veux quel s'affiche en dessous des embeds
 const msgconsole = "sltcv"; //ton message personalisé das la console
